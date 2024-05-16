fb609fbe5f112b85dfa55dd2e6363638c193f65f
Dim3ji <fataibada@gmail.com>
2024-05-16 18:57:31 +0100

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: '06/01/2024',
+  birthdate: '05/16/2024',
   name: 'Benita'
 };
 
